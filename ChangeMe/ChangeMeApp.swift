e9ec1a5326384f29ff6f17b99c9b8a3b08fed1a3
WessoBesso <wessam1847@gmail.com>
2026-07-22 21:41:59 -0400

    Initial Commit

---
+++ ChangeMe/ChangeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeMeApp.swift
+//  ChangeMe
+//
+//  Created by WessoBesso on 2026-07-22.
+//
+
+import SwiftUI
+
+@main
+struct ChangeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChangeMeTests/ChangeMeTests.swift
@@ -0,0 +1,19 @@
+//
+//  ChangeMeTests.swift
+//  ChangeMeTests
+//
+//  Created by WessoBesso on 2026-07-22.
+//
+
+import Testing
+@testable import ChangeMe
+
+struct ChangeMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
